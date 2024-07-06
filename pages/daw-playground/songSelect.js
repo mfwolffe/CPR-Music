fae0322950fdc53a90b19b2a9fdb952b4ccbe135
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-07-05 21:36:53 -0400

    loader for backend daw tracks

---
+++ pages/daw-playground/songSelect.js
@@ -0,0 +1,29 @@
+import Form from 'react-bootstrap/Form';
+import { fetchSongs } from '../../actions';
+import Spinner from 'react-bootstrap/Spinner';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function SongSelector({ onChange }) {
+  let { items: songs, loaded: songsLoaded } = useSelector(
+    (state) => state.songs
+  );
+
+  if (!songsLoaded) {
+    console.log('loading songs');
+    const dispatch = useDispatch();
+    songs = dispatch(fetchSongs());
+  }
+
+  return songsLoaded ? (
+    <Form.Select onChange={(e) => onChange(songs[e.target.value])}>
+      {songs &&
+        Object.values(songs).map((song, i) => (
+          <option key={song.id} value={i}>
+            {song.title}
+          </option>
+        ))}
+    </Form.Select>
+  ) : (
+    <Spinner size="sm" variant="primary" />
+  );
+}
