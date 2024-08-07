d8808168467f86a72742ddb56f4562fb1259b48a
mfwolffe <wolffemf@dukes.jmu.edu>
2024-08-07 15:52:57 -0400

    init simple top control bar

---
+++ components/daw/simpleControlsTop.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from 'react-bootstrap/Button';
+
+import { BsZoomIn } from 'react-icons/bs';
+import { BsZoomOut } from 'react-icons/bs';
+import { TbZoomReset } from 'react-icons/tb';
+
+const { useMemo, useState, useCallback, useRef, useEffect } = React;
