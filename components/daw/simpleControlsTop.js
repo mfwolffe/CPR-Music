d45540a6502d9c1ef68abd374a98b9ff262a6086
mfwolffe <wolffemf@dukes.jmu.edu>
2024-08-08 22:48:06 -0400

    resume attemps at undo/redo

--- components/daw/simpleControlsTop.js
+++ components/daw/simpleControlsTop.js
@@ -3,6 +3,8 @@ import Button from 'react-bootstrap/Button';
 import { Spinner } from 'react-bootstrap';
 
 import { MdGroups } from 'react-icons/md';
+import { IoArrowUndo } from 'react-icons/io5';
+import { IoArrowRedo } from 'react-icons/io5';
 import { IoCutOutline } from 'react-icons/io5';
 import { MdOutlineWaves } from 'react-icons/md';
 import { RiEqualizerLine } from 'react-icons/ri';
@@ -23,6 +25,9 @@ const SimpleDawControlsTop = ({
   ffmpegLoaded,
   chrPresent,
   chrSetter,
+  editIndex,
+  editList,
+  restoreState,
 }) => {
   if (!waveSurfer) return '';
 
@@ -49,74 +54,90 @@ const SimpleDawControlsTop = ({
 
   const dawSpinner = <Spinner animation="grow" size="sm" />;
 
+  const length = editList.length;
+
   return (
     <>
-      <div className="d-flex w-100 ml-auto mr-auto pl-2 toolbar align-items-center flex-row gap-0375">
-        <Button className="prog-button" onClick={handleMinimap}>
-          <MdOutlineWaves
-            fontSize="1rem"
-            onPointerEnter={() => setMapHvr(true)}
-            onPointerLeave={() => setMapHvr(false)}
-            style={{ color: mapPresent || mapHvr ? 'aqua' : 'white' }}
-          />
-        </Button>
-
-        <Button className="prog-button" onClick={toggleEQ}>
-          <RiEqualizerLine
-            fontSize="1rem"
-            onPointerEnter={() => setEqHvr(true)}
-            onPointerLeave={() => setEqHvr(false)}
-            style={{ color: eqPresent || eqHvr ? 'aqua' : 'white' }}
-          />
-        </Button>
-
-        <Button className="prog-button" onClick={toggleRvb}>
-          {ffmpegLoaded ? (
-            <RiSoundModuleFill
+      <div className="d-flex w-100 ml-auto mr-auto pl-2 toolbar align-items-center flex-row flex-between gap-0375">
+        <div className="d-flex gap-0375 align-items-center">
+          <Button className="prog-button" onClick={handleMinimap}>
+            <MdOutlineWaves
               fontSize="1rem"
-              onPointerEnter={() => setRvbHvr(true)}
-              onPointerLeave={() => setRvbHvr(false)}
-              style={{ color: rvbPresent || rvbHvr ? 'aqua' : 'white' }}
+              onPointerEnter={() => setMapHvr(true)}
+              onPointerLeave={() => setMapHvr(false)}
+              style={{ color: mapPresent || mapHvr ? 'aqua' : 'white' }}
             />
-          ) : (
-            dawSpinner
-          )}
-        </Button>
-
-        <Button className="prog-button" onClick={() => toggleChorus(cutRegion)}>
-          {ffmpegLoaded ? (
-            <MdGroups
-              onPointerEnter={() => setChrHvr(true)}
-              onPointerLeave={() => setChrHvr(false)}
-              style={{ color: chrPresent || chrHvr ? 'aqua' : 'white' }}
+          </Button>
+          <Button className="prog-button" onClick={toggleEQ}>
+            <RiEqualizerLine
               fontSize="1rem"
+              onPointerEnter={() => setEqHvr(true)}
+              onPointerLeave={() => setEqHvr(false)}
+              style={{ color: eqPresent || eqHvr ? 'aqua' : 'white' }}
             />
-          ) : (
-            dawSpinner
-          )}
-        </Button>
-
-        <Button className="prog-button">
-          {ffmpegLoaded ? (
-            <IoCutOutline
-              fontSize="1rem"
-              onClick={() => transcoder(cutRegion)}
-            />
-          ) : (
-            dawSpinner
-          )}
-        </Button>
-
-        <Button className="prog-button">
-          {ffmpegLoaded ? (
-            <IoTrashOutline
-              fontSize="1rem"
-              onClick={() => destroyRegion(cutRegion)}
-            />
-          ) : (
-            dawSpinner
-          )}
-        </Button>
+          </Button>
+          <Button className="prog-button" onClick={toggleRvb}>
+            {ffmpegLoaded ? (
+              <RiSoundModuleFill
+                fontSize="1rem"
+                onPointerEnter={() => setRvbHvr(true)}
+                onPointerLeave={() => setRvbHvr(false)}
+                style={{ color: rvbPresent || rvbHvr ? 'aqua' : 'white' }}
+              />
+            ) : (
+              dawSpinner
+            )}
+          </Button>
+          <Button
+            className="prog-button"
+            onClick={() => toggleChorus(cutRegion)}
+          >
+            {ffmpegLoaded ? (
+              <MdGroups
+                onPointerEnter={() => setChrHvr(true)}
+                onPointerLeave={() => setChrHvr(false)}
+                style={{ color: chrPresent || chrHvr ? 'aqua' : 'white' }}
+                fontSize="1rem"
+              />
+            ) : (
+              dawSpinner
+            )}
+          </Button>
+          <Button className="prog-button">
+            {ffmpegLoaded ? (
+              <IoCutOutline
+                fontSize="1rem"
+                onClick={() => transcoder(cutRegion)}
+              />
+            ) : (
+              dawSpinner
+            )}
+          </Button>
+          <Button className="prog-button">
+            {ffmpegLoaded ? (
+              <IoTrashOutline
+                fontSize="1rem"
+                onClick={() => destroyRegion(cutRegion)}
+              />
+            ) : (
+              dawSpinner
+            )}
+          </Button>
+        </div>
+        <div className="d-flex align-items-center">
+          <Button
+            className="prog-button pr-2"
+            onClick={() => restoreState(editIndex - 1)}
+          >
+            <IoArrowUndo fontSize="1rem" />
+          </Button>
+          <Button
+            className="prog-button pr-2"
+            onClick={() => restoreState(editIndex + 1)}
+          >
+            <IoArrowRedo fontSize="1rem" />
+          </Button>
+        </div>
       </div>
     </>
   );
