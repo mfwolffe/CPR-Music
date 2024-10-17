9fa78ee0d1af7105c38ca7c385272466b4e5f0f6
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-10-17 19:23:53 -0400

    at least no exception anymore

--- components/recorder.js
+++ components/recorder.js
@@ -1,8 +1,19 @@
 // with thanks to https://medium.com/front-end-weekly/recording-audio-in-mp3-using-reactjs-under-5-minutes-5e960defaf10
 
+import HelpAccordion from './audio/daw/dawHelp';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+import { fetchFile } from '@ffmpeg/util';
+import { GrHelpBook } from "react-icons/gr";
+import { useWavesurfer } from '@wavesurfer/react';
+import Zoom from 'wavesurfer.js/dist/plugins/zoom.esm.js';
+import Hover from 'wavesurfer.js/dist/plugins/hover.esm.js';
+import Minimap from 'wavesurfer.js/dist/plugins/minimap.esm.js';
+import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js';
 import MicRecorder from 'mic-recorder-to-mp3';
-import { useEffect, useRef, useState, useCallback } from 'react';
-import Button from 'react-bootstrap/Button';
+import { WidgetSlider, ReverbChorusWidget, EQSliders } from './audio/daw/control';
+import { useCallback, useMemo, useState, useRef, useEffect } from 'react';
+
 import {
   FaMicrophone,
   FaStop,
@@ -29,7 +40,36 @@ import { UploadStatusEnum } from '../types';
 import StatusIndicator from './statusIndicator';
 import styles from '../styles/recorder.module.css';
 
-import DawStd from './audio/daw/dawStd';
+// @mfwolffe
+import { loadFfmpeg, formatTime, formatTimeMilli, setupAudioContext, effectChorusReverb } from '../lib/dawUtils';
+import { Button, Card, CardBody, CardHeader, CardTitle, Modal, Spinner } from 'react-bootstrap';
+import { DawControlsBottom, DawControlsTop, MinimapContainer  } from '../components/audio/daw/common';
+
+const EQWIDTH = 28;
+const RVBWIDTH = 13;
+const CHRWIDTH = 18;
+
+// @mfwolffe
+const HelpModal = ({ setFn, shown }) => {
+  return (
+    <>
+      <Modal size='lg' show={shown} onHide={() => setFn(false)} style={{maxHeight: "96%"}}>
+        <Modal.Header style={{background: "var(--daw-timeline-bg)", color: "white" }}>
+          <Modal.Title>DAW Help</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{overflow: "scroll", backgroundColor: "var(--daw-grey"}}>
+          <HelpAccordion />
+        </Modal.Body>
+        <Modal.Footer style={{backgroundColor: "var(--daw-timeline-bg"}}>
+          <Button variant="primary" onClick={() => setFn(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
 
 function AudioViewer({ src }) {
   const containerW = useRef(null);
@@ -212,6 +252,18 @@ function AudioViewer({ src }) {
 }
 
 export default function Recorder({ submit, accompaniment }) {
+  let zoom, hover, minimap, timeline, regions;
+  let disableRegionCreate;
+
+  const [filters, setFilters] = useState([]);
+
+  let audio;
+
+  const dawRef = useRef(null);
+  const audioRef = useRef(null);
+
+  const ffmpegRef = useRef(new FFmpeg());
+
   // const Mp3Recorder = new MicRecorder({ bitRate: 128 }); // 128 is default already
   const [isRecording, setIsRecording] = useState(false);
   const [blobURL, setBlobURL] = useState('');
@@ -223,9 +275,197 @@ export default function Recorder({ submit, accompaniment }) {
   const [min, setMinute] = useState(0);
   const [sec, setSecond] = useState(0);
 
+  // @mfwolffe | effects
+  const [decay, setDecay] = useState(0);
+  const [delay, setDelay] = useState(0);
+  const [inGain, setInGain] = useState(0);
+  const [outGain, setOutGain] = useState(0);
+  const [speedChr, setSpeedChr] = useState(0);
+  const [delayChr, setDelayChr] = useState(0);
+  const [decayChr, setDecayChr] = useState(0);
+
+  // @mfwolffe 
+  const [audioURL, setAudioURL] = useState("");
+  
+  // @mfwolffe
+  const [loaded, setLoaded] = useState(false);
+  const [depthsChr, setDepthsChr] = useState(0);
+  const [inGainChr, setInGainChr] = useState(0);
+  const [cutRegion, setCutRegion] = useState('');
+  const [showHelp, setShowHelp] = useState(false);
+  const [outGainChr, setOutGainChr] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [eqPresent, setEqPresent] = useState(false);
+  const [mapPresent, setMapPrsnt] = useState(false);
+  const [editList, setEditList] = useState([]);
+  const [rvbPresent, setRvbPresent] = useState(false);
+  const [chrPresent, setChrPresent] = useState(false);
+  const [playbackSpeed, setPlaybackSpeed] = useState(1);
+  const [editListIndex, setEditListIndex] = useState(0);
+
+  const chorusSliders = [
+    WidgetSlider(0, 1, 0.001, 0, setInGainChr, 'Input'),
+    WidgetSlider(0, 1, 0.001, 0, setOutGainChr, 'Output'),
+    WidgetSlider(0, 70, 0.1, 0, setDelayChr, 'Delay'),
+    WidgetSlider(0.01, 1, 0.001, 0.01, setDecayChr, 'Decay'),
+    WidgetSlider(0.1, 90000.0, 0.1, 1000, setSpeedChr, 'Speed'),
+    WidgetSlider(0.01, 4, 0.001, 1, setDepthsChr, 'Depth'),
+  ];
+
+  const reverbSliders = [
+    WidgetSlider(0, 1, 0.001, 0, setInGain, 'Input'),
+    WidgetSlider(0, 1, 0.001, 0, setOutGain, 'Output'),
+    WidgetSlider(0.1, 90000.0, 1, 1000, setDelay, 'Delay'),
+    WidgetSlider(0.1, 1, 0.001, 0.1, setDecay, 'Decay'),
+  ];
+
+  useEffect(() => {
+    console.log('blobs', editList);
+  }, [editList]);
+
   // @mfwolffe
   const [showDAW, setShowDAW] = useState(false);
+  // const [audio, setAudio] = useState(null);
+  // SEEME @mfwolffe try useEffect on dawURL then setupAudioContext if no dice.
 
+  useEffect(() => {
+    if (audioURL === "") return;
+    console.log("daudio", audioURL);
+    let tempAudioObj = setupAudioContext(audioURL);
+
+    audio = tempAudioObj.audio;
+    setFilters([...tempAudioObj.filters]);
+
+    audioRef.current = audio;
+    audioRef.current.src = audioURL;
+  }, [audioURL]);
+
+
+  useEffect(() => {
+    if (!filters || !filters.length) return;
+    setShowDAW(true);
+  }, [filters])
+
+  // @mfwolffe
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
+    height: 208,
+    media: audio,
+    barHeight: 0.8,
+    cursorWidth: 2,
+    autoScroll: true,
+    dragToSeek: true,
+    container: dawRef,
+    waveColor: '#7bafd4',
+    cursorColor: 'var(--jmu-gold)',
+    hideScrollbar: false,
+    progressColor: '#92ce84',
+    plugins: useMemo(() => [], []),
+  });
+
+  // @mfwolffe
+  wavesurfer?.once('ready', () => {
+    if (wavesurfer.getActivePlugins().length === 0) {
+      zoom = wavesurfer?.registerPlugin(
+        Zoom.create({
+          deltaThreshold: 5,
+          maxZoom: 150,
+          scale: 0.125,
+        })
+      );
+
+      hover = wavesurfer?.registerPlugin(
+        Hover.create({
+          lineWidth: 2,
+          labelSize: 12,
+          labelColor: '#fff',
+          formatTimeCallback: formatTime,
+          lineColor: 'var(--jmu-gold)',
+        })
+      );
+
+      minimap = wavesurfer?.registerPlugin(
+        Minimap.create({
+          height: 35,
+          dragToSeek: true,
+          container: '#mmap',
+          waveColor: '#b999aa',
+          cursorColor: 'var(--jmu-gold)',
+          progressColor: '#92ceaa',
+          cursorWidth: 2,
+        })
+      );
+
+      timeline = wavesurfer?.registerPlugin(
+        Timeline.create({
+          height: 24,
+          insertPosition: 'beforebegin',
+          style: 'color: #e6dfdc; background-color: var(--daw-timeline-bg)',
+        })
+      );
+
+      regions = wavesurfer?.registerPlugin(RegionsPlugin.create());
+      disableRegionCreate = regions?.enableDragSelection({
+        color: 'rgba(155, 115, 215, 0.4)', // FIXME @mfwolffe color param has no effect
+      });
+      regions?.on('region-created', (region) => {
+        disableRegionCreate();
+        setCutRegion(region);
+      });
+      regions?.on('region-double-clicked', (region) => {
+        region.remove();
+        disableRegionCreate = regions.enableDragSelection();
+      });
+    }
+
+    if (!loaded) loadFfmpeg(ffmpegRef, setLoaded, setIsLoading);
+  });
+
+  useEffect(() => {
+    async function updatePlaybackSpeed() {
+      const ffmpeg = ffmpegRef.current;
+      await ffmpeg.writeFile('input.mp3', await fetchFile(ORIGURL));
+      await ffmpeg.exec([
+        '-i',
+        'input.mp3',
+        '-af',
+        `atempo=${playbackSpeed}`,
+        'output.mp3',
+      ]);
+
+      const data = await ffmpeg.readFile('output.mp3');
+      if (audioRef.current) {
+        audioRef.current.src = URL.createObjectURL(
+          new Blob([data.buffer], { type: 'audio/mp3' })
+        );
+      }
+
+      setAudioURL(audioRef.current.src);
+      console.log('speed adjust done', audioRef.current.src);
+      wavesurfer.load(audioRef.current.src);
+    }
+
+    if (ffmpegRef.current.loaded) updatePlaybackSpeed();
+  }, [playbackSpeed]);
+
+  const params = {
+    audioRef: audioRef,
+    setAudioURL: setAudioURL,
+    audioURL: audioURL,
+    wavesurfer: wavesurfer,
+    setEditList: setEditList,
+    editList: editList,
+    setEditListIndex: setEditListIndex,
+    editListIndex: editListIndex,
+    hasButton: true,
+    ffmpegRef: ffmpegRef,
+    ffmpegLoaded: loaded,
+    handler: effectChorusReverb,
+  };
+
+  const handleHelp = useCallback(() => {
+    setShowHelp(true)
+  });
+  
   const accompanimentRef = useRef(null);
 
   const router = useRouter();
@@ -363,6 +603,7 @@ export default function Recorder({ submit, accompaniment }) {
           {blobInfo.length === 0 ? (
             <span>No takes yet. Click the microphone icon to record.</span>
           ) : (
+            <>
             <ListGroup as="ol" numbered>
               {blobInfo.map((take, i) => (
                 <ListGroupItem
@@ -391,17 +632,102 @@ export default function Recorder({ submit, accompaniment }) {
                   <div className="minWidth">
                     <StatusIndicator statusId={`recording-take-${i}`} />
                   </div>
+                  <div>
+                    <Button variant='primary' onClick={ () => { setAudioURL(take.url); } }>Edit Take</Button>
+                  </div>
                 </ListGroupItem>
               ))}
             </ListGroup>
+            </>
           )}
           {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-
-          {/* @mfwolffe */}
-          { blobInfo.length === 0 ? ( <Button variant='primary' onClick={ () => setShowDAW(true) } >Edit Take</Button>) : <></> }
-          { showDAW == true ? ( <DawStd takeURL={take.url}></DawStd> ) : ( <></> ) }
-
+          {/* { showDAW == true ? ( <DawStd takeURL={blobURL}></DawStd> ) : ( <></> ) } */}
           <audio src={blobURL} />
+
+      <HelpModal setFn={setShowHelp} shown={showHelp} />
+
+      <Card className="mt-2 mb-2">
+        <CardHeader className="pt-1 pb-1 flex-between">
+          <CardTitle className="pt-0 pb-0 mt-0 mb-0">Audio Editor</CardTitle>
+          <Button className='help-button daw-help align-center' onClick={() => handleHelp()}>
+            <GrHelpBook className="help-ico" fontSize="1.5rem" />
+          </Button>
+        </CardHeader>
+        <CardBody>
+
+
+          { showDAW ? 
+          
+          <div className="d-flex w-100 gap-2p">
+            <div
+              id="waveform-container"
+              style={{
+                width: `${
+                  100 -
+                  (rvbPresent || eqPresent || chrPresent ? 1.5 : 0) -
+                  (eqPresent ? EQWIDTH : 0) -
+                  (rvbPresent ? RVBWIDTH : 0) -
+                  (chrPresent ? CHRWIDTH : 0)
+                }%`,
+              }}
+            >
+              <DawControlsTop
+                mapPresent={mapPresent}
+                mapSetter={setMapPrsnt}
+                eqSetter={setEqPresent}
+                eqPresent={eqPresent}
+                cutRegion={cutRegion}
+                rvbPresent={rvbPresent}
+                rvbSetter={setRvbPresent}
+                chrPresent={chrPresent}
+                chrSetter={setChrPresent}
+                {...params}
+              />
+              <div
+                ref={dawRef}
+                id="waveform"
+                className="ml-auto mr-auto mb-0 mt-0"
+              />
+              {MinimapContainer(!mapPresent)}
+              <DawControlsBottom
+                wavesurfer={wavesurfer}
+                playbackSpeed={playbackSpeed}
+                speedSetter={setPlaybackSpeed}
+              />
+            </div>
+
+            <EQSliders hide={!eqPresent} filters={filters} width={EQWIDTH} />
+
+            <ReverbChorusWidget
+              hide={!rvbPresent}
+              width={RVBWIDTH}
+              sliders={reverbSliders}
+              title={'Reverb'}
+              inGainChr={inGain}
+              outGainChr={outGain}
+              delayChr={delay}
+              decayChr={decay}
+              speedChr={null}
+              depthsChr={null}
+              {...params}
+            />
+            <ReverbChorusWidget
+              hide={!chrPresent}
+              width={CHRWIDTH}
+              sliders={chorusSliders}
+              title={'Chorus'}
+              inGainChr={inGainChr}
+              outGainChr={outGainChr}
+              delayChr={delayChr}
+              decayChr={decayChr}
+              speedChr={speedChr}
+              depthsChr={depthsChr}
+              {...params}
+            />
+          </div> : <Spinner /> }
+
+        </CardBody>
+      </Card>
         </Col>
       </Row>
     </>
