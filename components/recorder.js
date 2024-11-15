8bbf80ecc3cd997cafd6f0202eaf249a47fd74bf
Matt Wolffe <wolffemf@dukes.jmu.edu>
2024-11-14 21:59:29 -0500

    load take in daw automatically

--- components/recorder.js
+++ components/recorder.js
@@ -707,6 +707,8 @@ export default function Recorder({ submit, accompaniment }) {
             ]);
             setIsRecording(false);
             chunksRef.current = [];
+            setAudioURL(url);
+            setTakeNo(takeNo + 1);
           };
 
           setMediaRecorder(recorder);
